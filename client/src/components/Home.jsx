757e2f140d3809b4877fcc25b13b4db87022d39f
krithika117 <krithikanithyanandam@gmail.com>
2022-06-25 11:51:07 +0530

    landing page changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,22 +3,20 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs"
 
 import { Loader } from './'
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border -gray-400 "
-const Home = () => {
-    const connectWallet = () => {
 
-    }
+const Home = () => {
+
     return (
-        <div className="flex w-full justify-center items-center">
+        <div className="flex w-full justify-center items-center p-6 mb-12" style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '85vh',
+
+        }}>
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div style={{
-                    display: 'flex',
-                    fontSize: '20vh',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '50vh',
-                }}>
-                    <h1 className="text-black text-gradient t-shadow" style={{ fontWeight: 'bold' }}>kriptoweb.</h1>
+                <div>
+                    <h1 id="headerID" className="text-black text-gradient t-shadow">kriptoweb.</h1>
                     {/* <p className="text-left mt-5 text-black font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and sell cryptocurrencies easily on Kriptoweb.</p>
                     <button
                         type="button"
