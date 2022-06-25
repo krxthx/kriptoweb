f7633e50d2ce5e877dfe490b509f1d9744b75295
krithika117 <krithikanithyanandam@gmail.com>
2022-06-25 14:43:18 +0530

    navbar slide in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { HiMenuAlt4 } from 'react-icons/hi';
+import { HiMenu } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png'
-const NavBarItem = ({ title, classProps }) => {
+const NavbarItem = ({ title, classProps }) => {
     return (
         < li className={`mx-4 cursor-pointer ${classProps}`}>
             {title}
@@ -17,27 +17,30 @@ const Navbar = () => {
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src={logo} alt="KRIPTOWEB" class="w-32 cursor-pointer" />
             </div>
-            <ul className='text-black md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+            {/* <ul className='text-black md:flex hidden list-none flex-row justify-between items-center flex-initial'>
                 {["Market", "Exchange", "Tutorial", "Wallet"].map((item, index) => (
-                    <NavBarItem key={item + index} title={item} />
+                    <NavbarItem key={item + index} title={item} />
                 ))}
                 <li className='bg-[#212121] text-white py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#3e3e3e]'>Login</li>
-            </ul>
+            </ul> */}
             <div className="flex relative">
                 {toggleMenu
-                    ? <AiOutlineClose fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
-                    : <HiMenuAlt4 fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />}
+                    ? <AiOutlineClose className="text-black text-xl cursor-pointer" onClick={() => setToggleMenu(false)} />
+                    : <HiMenu className="text-black text-xl cursor-pointer" onClick={() => setToggleMenu(true)} />}
                 {toggleMenu && (
                     <ul
-                        className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none 
-                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
+                        className='z-10 fixed top-0 -right-2 p-3 w-full h-screen shadow-2xl list-none 
+                        flex flex-col justify-start items-end rounded-md bg-white'
                     >
-                        <li className='text-xl w-full my-2'>
-                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                        <li className='text-xl w-full my-2 animate-slide-out'>
+                            <AiOutlineClose  onClick={() => setToggleMenu(false)} />
                         </li>
-                        {["Market", "Exchange", "Tutorial", "Wallet"].map((item, index) => (
-                            <NavBarItem key={item + index} title={item} classProps="my-2 text-lg" />
+                        {["Login", "Market", "Exchange", "Tutorial", "Wallet"].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
                         ))}
+                        <li>
+                            <p className="p-3">Developed by Krithika Nithyanandam</p>
+                        </li>
                     </ul>
                 )}
             </div>
